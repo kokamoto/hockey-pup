d6e9b0ea5a17a807da2f823c900e7874ab8c6379
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2025-06-07 10:32:57 -0700

    Fixing hockey-pup.svg

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
 import { Layout, type LayoutProps } from './Layout.tsx'
 
 import examples from './pages/examples.tsx'
 import nhl from './pages/nhl.tsx'
 
 const app = new Hono();
+
+app.use('/static/*', serveStatic({ root: './src/' }))
 
 app.get('/', (c) => {
   // Render the main page with a layout
@@ -18,6 +21,7 @@ app.get('/', (c) => {
       <h1>Welcome to Hockey Pup!</h1>
       <p>Your hockey companion</p>
       <p>Visit <a href="/examples">/examples</a> to see another route.</p>
+      <img src="/static/hockey-pup.svg" alt="Hockey Pup Logo" />
     </Layout>)
 })
 
