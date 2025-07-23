6530906908f35a26f15444171aa005642553fad8
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2025-07-23 10:16:24 -0700

    Add tests for Config module to validate database file path based on environment

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,8 @@
 const DATABASE_FILE_PATH = "data/hockey-pup.sqlite";
+const TEST_DATABASE_FILE_PATH = "data/hockey-pup.test.sqlite";
 
 function getDataBaseFilePath(): string {
-  return process.env.DATA_BASE_FILE_PATH || DATABASE_FILE_PATH
+  return process.env.HOCKEY_PUP_ENV === "test" ? TEST_DATABASE_FILE_PATH : DATABASE_FILE_PATH;
 }
 
 export const Config = {
