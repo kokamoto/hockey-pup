ee8b0f7038ffea0c6efb48deecb7635b6fb02e44
Kevin Okamoto <kevin.k.okamoto@gmail.com>
2025-09-06 15:51:33 -0700

    Add SQL table creation script and update database initialization logic

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 const DATABASE_FILE_PATH = "data/hockey-pup.sqlite";
 const TEST_DATABASE_FILE_PATH = "data/hockey-pup.test.sqlite";
 
-function getDataBaseFilePath(): string {
-  return process.env.HOCKEY_PUP_ENV === "test" ? TEST_DATABASE_FILE_PATH : DATABASE_FILE_PATH;
+function getDataBaseFilePath(env?: string): string {
+  return env === "test" ? TEST_DATABASE_FILE_PATH : DATABASE_FILE_PATH;
 }
 
 export const Config = {
